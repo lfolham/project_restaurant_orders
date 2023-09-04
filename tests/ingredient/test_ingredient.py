bbfad7dedd192fe0b74accb7ab6383ce7b81ffc4
lfolham <lucasfolha@icloud.com>
2023-09-04 15:26:15 -0300

    task 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,19 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("bacon")
+
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient2)
+    assert ingredient1 != ingredient2
+    assert repr(ingredient1) != "Ingredient('mussarela')"
+    assert ingredient1.name != "queijo mussarela"
+
+    incorrect_restrictions = {Restriction.LACTOSE}
+    assert ingredient1.restrictions == incorrect_restrictions
