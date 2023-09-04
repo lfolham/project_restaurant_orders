18b0ed01d36436cfb73324f246b9e61e5a416f5e
lfolham <lucasfolha@icloud.com>
2023-09-04 16:38:27 -0300

    task 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,19 +1,19 @@
 from src.models.ingredient import (
     Ingredient,
-    Restriction,
+    restriction_map,
 )  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
     ingredient1 = Ingredient("queijo mussarela")
-    ingredient2 = Ingredient("bacon")
+    ingredient2 = Ingredient("queijo gorgonzola")
 
-    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) == hash(ingredient1)
     assert hash(ingredient1) != hash(ingredient2)
+    assert ingredient1 == ingredient1
     assert ingredient1 != ingredient2
-    assert repr(ingredient1) != "Ingredient('mussarela')"
-    assert ingredient1.name != "queijo mussarela"
-
-    incorrect_restrictions = {Restriction.LACTOSE}
-    assert ingredient1.restrictions == incorrect_restrictions
+    assert repr(ingredient1) == "Ingredient('queijo mussarela')"
+    assert ingredient1.name == "queijo mussarela"
+    correct_restrictions = restriction_map().get("queijo mussarela", set())
+    assert ingredient1.restrictions == correct_restrictions
